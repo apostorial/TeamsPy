989a53c8adf0f0e62dfb610a0e00b0a80b9b913d
Apostorial <72036855+Apostorial@users.noreply.github.com>
2021-06-15 07:43:13 -0700

    New feature added
    Now the bot can stay in the reunion for a period of your choice, and leaves after. Also added some print statements that describe what the bot does

--- main.py
+++ main.py
@@ -33,8 +33,9 @@ while True:
 driver = Chrome()
 
 driver.get("https://outlook.live.com/owa/")
-
+print("Google Chrome opened.")
 driver.find_element(By.LINK_TEXT, "Sign in").click()
+print("Login page joined.")
 time.sleep(1)
 file2 = open("emailpass.txt", "r")
 line2 = file2.readline()
@@ -43,21 +44,28 @@ email = emailPass[0]
 password = emailPass[1]
 
 driver.find_element(By.ID, "i0116").send_keys(f"{email}\n")
+print("Email pasted.")
 time.sleep(5)
 pyautogui.write(f"{password}\n")
+print("Password pasted.")
 time.sleep(1)
 
 if "Stay signed in?" in driver.page_source:
     driver.find_element(By.ID, "idSIButton9").click()
+    print("Successfully bypassed 'Stay signed in'.")
 else:
+    print("'Stay signed in' not found.")
     pass
 
 time.sleep(1)
 driver.find_element(By.CLASS_NAME, "_3lIR1LfENBYPLTyCDNhq5k").click()
+print("The filter menu successfully opened.")
 time.sleep(1)
 driver.find_element(By.NAME, "Unread").click()
+print("Mails filtered.")
 time.sleep(1)
 driver.find_element(By.CLASS_NAME, "_2miAFnGHXlWwulyUmLEbzZ").click()
+print("Mail opened.")
 time.sleep(1)
 
 file3 = open("invitesentence.txt", "r", encoding="utf8")
@@ -66,12 +74,16 @@ line3 = file3.readline()
 def checkLink():
     try:
         driver.find_element(By.LINK_TEXT, f"{line3}").click()
+        print("Joining link found.")
     except NoSuchElementException:
         driver.find_element_by_css_selector('button[aria-label="More mail actions"]').click()
+        print("Action menu successfully opened.")
         time.sleep(1)
         driver.find_element(By.NAME, "Mark as read").click()
+        print("Mail read.")
         time.sleep(1)
         driver.find_element(By.CLASS_NAME, "_2miAFnGHXlWwulyUmLEbzZ").click()
+        print("Mail opened.")
         checkLink()
 
 checkLink()
@@ -82,21 +94,36 @@ pyautogui.press('enter')
 pyautogui.press('tab')
 pyautogui.press('tab')
 pyautogui.press('enter')
+print("Microsoft Teams is loading.")
 time.sleep(35)
 pyautogui.press('tab')
 pyautogui.press('tab')
 pyautogui.press('enter')
-time.sleep(3)
+print("Microphone and camera allowed.")
+time.sleep(6)
 pyautogui.press('tab')
 pyautogui.press('enter')
 pyautogui.press('tab')
 pyautogui.press('enter')
+print("Microphone and camera disabled.")
 time.sleep(2)
 
 joinNow = pyautogui.locateOnScreen("images/joinNow.png")
 if joinNow is not None:
     joinNowLocation = pyautogui.center(joinNow)
     pyautogui.click(joinNowLocation)
+    print("Reunion joined.")
 
+file4 = open("totaltime.txt", "r")
+line4 = float(file4.readline())
+
+inReunionTime = line4 * 3600
+
+for i in range(inReunionTime - 1):
+    time.sleep(1)
+    print(f"{i + 1} seconds passed.")
+
+driver.quit()
+print("Reunion left.")
 if __name__ == '__main__':
     pass
